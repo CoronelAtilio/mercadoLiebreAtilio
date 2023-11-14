e6f883c5b03f92ccb2ce24d753b5ea9594a337ec
CoronelAtilio <maxi-935@hotmail.com>
2023-11-14 16:23:26 -0300

    shadows

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const port = process.env.PORT || 4000;
+let port = 4000;
 
 app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname,'./views/index.html'));
