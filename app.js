a9694c179b49d9effbff686910366e5cff8c3b54
CoronelAtilio <maxi-935@hotmail.com>
2023-11-26 22:19:51 -0300

    Hasta la clase 20 full

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-let port = 4000;
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 4000;
+const carpetaPublic = path.resolve(__dirname, "./public");
+app.use(express.static(carpetaPublic));
 
-app.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname,'./views/index.html'));
-});
-
-app.listen(port, ()=>{
-    console.log(`
-    Nuestra app funciona en
-    http://localhost:${port}`);
+app.listen(port,()=>{
+    console.log(`puerto ${port} funcionando`);
 })
 
-const carpetaPublic = path.resolve(__dirname,'./public')
-app.use(express.static(carpetaPublic));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))    
+})
